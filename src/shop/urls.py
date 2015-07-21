d085de3c3223580605c5598d6d0b1f4509ccfb4e
D1onisko <d1oniskozak@gmail.com>
2015-07-21 22:43:32 +0300

    вывод товаров в категорииях, но только без поля slug(по pk ключу)

--- src/shop/urls.py
+++ src/shop/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('src.shop.views',
 
     url(r'^$', 'index', name='index'),
-    url(r'^product/(?P<slug>[-\w]+)/$', 'product_detail', name='product_detail'),
+    url(r'^category/(?P<pk>[^/]+)/$', 'category', name='category'),
+    url(r'^product/(?P<pk>[^/]+)/$', 'product_detail', name='product_detail'),
 
 
 )
